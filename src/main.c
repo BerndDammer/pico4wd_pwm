d7d6b6290635ec228c9912d4c6a2d2af61291ed6
BerndDammer <BerndDammer@web.de>
2023-02-15 21:17:52 +0100

    compilation success

--- src/main.c
+++ src/main.c
@@ -6,10 +6,6 @@
 #include "hardware/watchdog.h"
 #include "hardware/clocks.h"
 
-int64_t alarm_callback(alarm_id_t id, void *user_data) {
-    // Put your timeout handler code in here
-    return 0;
-}
 
 
 
